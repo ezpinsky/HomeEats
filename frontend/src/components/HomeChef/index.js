f96505b13540844a66ccca655d8cb4ffc8e1e590
ezpinsky <ezpinsky@gmail.com>
2020-11-29 23:26:49 -0500

    fixes homeChef component variable

--- frontend/src/components/HomeChef/index.js
+++ frontend/src/components/HomeChef/index.js
@@ -20,11 +20,7 @@ export default function HomeChef() {
 	}, [dispatch, homeChefId]);
 
 	const { homeChefsList } = useSelector(state => state.homeChefs);
-	// const { homeChef } = useSelector(state => state.homeChef);
-
-	let chef = homeChefsList.find(chef => chef.id === homeChefId);
-
-	const [theChef, setChef] = useState(chef);
+	const homeChef = useSelector(state => state.homeChefs.homeChef);
 
 	return (
 		<>
@@ -32,17 +28,17 @@ export default function HomeChef() {
 			<Navigation />
 			<div key={v4()} className='chef-container'>
 				<div className='image-container'>
-					<img src={`${chef.image}`} alt='home chef' className='chef-image' />
+					<img src={`${homeChef.image}`} alt='home chef' className='chef-image' />
 				</div>
 				<div className='chef-content-container'>
-					<NavLink to={`/home-chef/${chef.id}`}>
-						<h2 className='chef-name'>{chef.name}</h2>
+					<NavLink to={`/home-chef/${homeChef.id}`}>
+						<h2 className='chef-name'>{homeChef.name}</h2>
 					</NavLink>
 					<div className='cuisines'>
 						<span>Cuisines:</span>
 					</div>
 					<div className='description'>
-						<p>{chef.description}</p>
+						<p>{homeChef.description}</p>
 					</div>
 				</div>
 			</div>
